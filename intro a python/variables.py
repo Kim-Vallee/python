07db5d3e233ea0eb6bfcb3157626afd1f3caf67a
Etilawin <kimik812@gmail.com>
2015-10-14 16:31:16 +0200

    Ajout Des différents noms de variables impossibles à utiliser

--- intro a python/variables.py
+++ intro a python/variables.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3.4
+
+nom_de_la variable = 'valeur' # Attention, la variable est différente de la suivante:
+Nom_De_La_Variable = 'valeur' # En effet python est sensible à la casse
+
+#Impossible d'utiliser les mots suivants:
+'''and
+del
+from
+none
+true
+as
+elif
+global
+nonlocal
+try
+assert
+else
+if
+not
+while
+break
+except
+import
+or
+with
+class
+false
+in
+pass
+yield
+continue
+finally
+is
+raise
+def
+for
+lambda
+return'''
