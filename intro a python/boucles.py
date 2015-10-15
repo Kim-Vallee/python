2ef2c812e52b0bebb4ef92b530b49ab325fe93c9
Etilawin <kimik812@gmail.com>
2015-10-15 19:10:18 +0200

    Added some files in "intro à python" Started writing comments in english

---
+++ intro a python/boucles.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.4
+
+def p(*args):
+    print(*args)
+
+nb = 20
+i = 0
+while i <= nb:
+    p("Nombre numéro : {}".format(i))
+    i += 1
+
+chaine = "Coucou"
+for lettre in chaine: #Lettre takes successively the value of each of the letters one by one
+    if lettre in "aeiouyAEIOUY": #Another use of "in" word
+        p(lettre)
+    else:
+        p("*")
+
+while True:
+    sortir = input("Entrez Q pour sortir : ")
+    if sortir.lower() == "q":
+        p("bye")
+        break
+
+x = 1
+while x < 20:
+    if x % 3 == 0:
+        x += 4
+        p("on ajoute 4 x vaut {}".format(x))
+        continue
+    else:
+        x += 1
+        p(x)
---
+++ intro a python/fonctions.py
@@ -0,0 +1,6 @@
+def p(*args):
+    print(*args)
+
+print('un petit mot')
+ma_variable = 53
+print(ma_variable)
