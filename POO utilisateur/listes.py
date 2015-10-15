12a9f7f0f88b49995c7891844dae6b316fda4777
Etilawin <kimik812@gmail.com>
2015-10-15 20:17:09 +0200

    Removed To let the renamed one taking the place

--- POO utilisateur/listes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3.4
-
-def p(*args):
-    print(args)
-
-#créations de listes
-liste = list()
-liste = []
-liste = [2 , 2.5, 'hello', []]
--- intro a python/modularites/print.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3.4
-
-def p(*args):
-    print(*args)
