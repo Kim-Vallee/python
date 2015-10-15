c6d60378fdf18ca504614cad28ccc779953445d7
Etilawin <kimik812@gmail.com>
2015-10-15 18:44:35 +0200

    Changed comments to a better visual

--- POO utilisateur/str.py
+++ POO utilisateur/str.py
@@ -4,29 +4,25 @@ def p(*args):
     print(*args)
 
 chaine = 'coucou mon frère'
-chaine.upper() #Va mettre la chaîne en majuscule grâce
-               #Upper est une fonction et le "." permet (comme avec les modules) d'exprimer l'appartenance
-               #applicable sur TOUTE les chaînes de caractères
+chaine.upper() #Met en majuscule
 
 type(chaine)   #Donne <class 'str'>
     # un objet est issu d'une classe <---- IMPORTANT*
     # La classe est une forme de type de donnée, sauf qu'elle permet de définir des fonctions et variables propres au type
-    #Les objets, que j'ai présentés comme des variables, pouvant contenir d'autres variables ou fonctions (que l'on appelle méthodes).
-    # On appelle une méthode d'un objet grâce à objet.methode()
+    # Les objets, que j'ai présentés comme des variables, pouvant contenir d'autres variables ou fonctions (que l'on appelle méthodes).
+
+
 # ---------------- Exemples de modules pour str ------------------------
+
 chaine.lower() #met en minuscule
 chaine.capitalize() #Met la première lettre en majuscule
 chaine.center(50).encode() #Met au milieu avec 50 la taille finale qu'on souhaite obtenir
-                           #On peut aussi voir que l'on peut ajouter deux methodes à la suite
+
 # ---------------- La méthode format -----------------------------------
+
 zero = '.'
 un = 26
-formation = "ceci est un texte{0} Il se compose de exactement {1} lettres".format(zero, un) #Qq explications:
-                        #Si tu te souviens pas, la methode format permet que lorsque Python exécute cette méthode,
-                        #il remplace dans notre chaîne {0} par la première variable passée à la méthode format
-                        #, {1} par la deuxième variable… et ainsi de suite.
-                        # On pourrait aussi faire:
-#formation = "ceci est un texte{} Il se compose de exactement {} lettres".format(zero, un)
+formation = "ceci est un texte{0} Il se compose de exactement {1} lettres".format(zero, un)
 formation2 ="""
  {no_rue}, {nom_rue}
     {code_postal} {nom_ville} ({pays})""".format(no_rue=5, nom_rue="rue des Postes", code_postal=75003, nom_ville="Paris", pays="France")
@@ -37,36 +33,36 @@ formation2 ="""
 
 age = 20
 part1 = 'J\'ai actuellement'
-final = part1 + str(age) + 'ans!' #On peut additionner des châines cependant les classes 'int' doivent être converties grâce à str(nombre)
-final == 'J\ai actuellement 20 ans!' #La chaîne complète donne ça
-final[0] #Donne la première lettre de final
-final[-1] #Donne la dernière lettre (de droite à gauche)
-len(final) #Donne la longueur de la chaine en caractères (Attention les espaces et tout comptent!
+final = part1 + str(age) + 'ans!'
+final[0]
+final[-1]
+len(final) #Donne la longueur de la chaine en caractères (Attention les espaces et tout comptent!)
 
 
 
 # ------------------ Parcourir une chaîne grâce à while/for ---------------
 
 chaine2 = "Salut"
-i = 0 # On appelle l'indice 'i' par convention
+i = 0
 while i < len(chaine2):
-    p(chaine2[i]) # On affiche le caractère à chaque tour de boucle (si je le fait avec p() cela m'affihera les tuples)
+    p(chaine2[i])
     i += 1
 
 # ------------------ Sélectionner une partie de la chaîne ------------------
 
 mot = "coucou"
 try:
-    mot[0] = "k" #Ne marche pas car on ne peut pas remplacer c par k de cette manière
+    mot[0] = "k" #Ne marche pas
 except:
-    print("En effet ça ne marche pas, on obtient un TypeError") #On affiche l'erreur
-    pass #On continue tout de même l'execution
+    print("En effet ça ne marche pas, on obtient un TypeError")
+    pass
 
 presentation = "hello :3"
-presentation[0:2] # On sélectionne les 2 premières lettres (le 0 est inclu mais le 2 est exclu)
-presentation[:2] # On fait pareil mais plus compact (ne marche que si on part de 0)
-presentation[2:len(presentation)] #On sélectionne la 3ème lettre jusqu'a la fin (Ici 2 n'est pas exclu)
-presentation[2:] # Pareil que la ligne 60 mais pour aller tout à la fin
 
-# Grâce aux indices nous pouvons maintenant changer la première lettre de la variable mot:
+presentation[0:2]
+presentation[:2]
+
+presentation[2:len(presentation)]
+presentation[2:]
+
 mot = "k" + mot[1:] # Et la ça marche
--- intro a python/variables.py
+++ intro a python/variables.py
@@ -2,41 +2,18 @@
 
 def p(*args):
     print(*args)
-    
-nom_de_la variable = 'valeur' # Attention, la variable est différente de la suivante:
-Nom_De_La_Variable = 'valeur' # En effet python est sensible à la casse
+
+#Deux variables différentes
+nom_de_la variable = 'valeur'
+Nom_De_La_Variable = 'valeur'
+float1 = 3.5
+int1 = 3
+str1 = 'hello'
+str2 = """ C'est
+sur
+plusieurs
+lignes """
+int2 += 3 # = 6
 
 #Impossible d'utiliser les mots suivants:
-'''and
-del
-from
-none
-true
-as
-elif
-global
-nonlocal
-try
-assert
-else
-if
-not
-while
-break
-except
-import
-or
-with
-class
-false
-in
-pass
-yield
-continue
-finally
-is
-raise
-def
-for
-lambda
-return'''
+'''and del from none true as elif global nonlocal try assert else if not while break except import or with class false in pass yield continue finally is raise def for lambda return'''
