c0d1aec24dcecdcb315fd2ab1e8feb8e9e99b85f
Etilawin <kimik812@gmail.com>
2015-10-21 17:46:48 +0200

    Added how for loops can be used with dictionnaries

--- POO_utilisateur/dictionnary.py
+++ POO_utilisateur/dictionnary.py
@@ -9,13 +9,13 @@ dictionnary = {}
 p(type(dictionnary))
 
 dictionnary["pseudo"] = 'Etilawin'
-dictionnary["password"] = 'password'
+dictionnary["password"] = '9568574531'
 dictionnary["email"] = 'kimik@hotmail.fr'
 p(dictionnary)
-del dictionnary["password"]
-p(dictionnary)
-old_email = dictionnary.pop('email') # Shorter than just before as it prints the value
-p(old_email)
+# del dictionnary["password"]
+# p(dictionnary)
+# old_email = dictionnary.pop('email') # Different because .pop method print the value which is deleted
+# p(old_email)
 
 def coucou():
     print('Voilà je suis la fonction la plus inutile du monde : coucou')
@@ -26,3 +26,14 @@ functions = {}
 functions['c'] = coucou
 functions['h'] = hibou
 functions['c']()
+
+for cle in dictionnary:
+    print(cle) # Will show keys in a random way but not their values
+
+for values in dictionnary.values():
+    print(values) # Now will show the values thx to method values
+
+for key,value in dictionnary.items():
+    print('Votre {} est : {}'.format(key,value)) # Shows both keys and values
+
+# About dictionnaries in functions look in fonctions.py line 59-61
